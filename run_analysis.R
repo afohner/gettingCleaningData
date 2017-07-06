c4fa4b981871a481077cfe99ef32c91c28397f62
afohner <afohner@uw.edu>
2017-07-06 09:38:14 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+              , destfile = 'Dataset.zip')
+unzip("Dataset.zip")
+
 #Part 1: merging test and training data sets
 #From working directory
 #read in common info
